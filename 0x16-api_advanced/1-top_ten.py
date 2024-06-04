59dc38323f140ba36d73baf8a333d276b4ca00ba
KALORSOR <alorsorgodsway@gmail.com>
2024-06-04 14:33:58 -0700

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,31 +3,45 @@
 """
 prints the titles of the first 10 hot posts listed for a given subreddit
 """
-
 from requests import get
-
+import requests
 
 def top_ten(subreddit):
-    """
-    function that queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
     params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
+    
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+    
+    if response.status_code != 200:
+        print(None)
+        return
 
     try:
-        my_data = results.get('data').get('children')
+        
+        data = response.json()
+        articles = data['data']['children']
 
-        for i in my_data:
-            print(i.get('data').get('title'))
+        
+        if not articles:
+            print(None)
+            return
 
-    except Exception:
-        print("None")
+        
+        for article in articles:
+            print(article['data']['title'])
+
+    except ValueError:
+        print(None)
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python3 0-main.py <subreddit>")
+    else:
+        subreddit = sys.argv[1]
+        top_ten(subreddit)
+
